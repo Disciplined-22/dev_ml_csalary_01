d18cac5c0e93a64df80f375424278d00cad1e9b0
Disciplined-22 <129745308+Disciplined-22@users.noreply.github.com>
2024-02-23 22:26:22 +0530

    Add files via upload

---
+++ evaluate.py
@@ -0,0 +1,13 @@
+from sklearn.metrics import mean_squared_error, r2_score
+import joblib
+import pandas as pd
+from train import predictions, X_train, X_test, y_train, y_test, train_model
+
+# Load the trained model
+# Calculate Mean Squared Error (MSE)
+mse = mean_squared_error(y_test, predictions)
+print("Mean Squared Error (MSE):", mse)
+
+# Calculate R-squared (coefficient of determination)
+r2 = r2_score(y_test, predictions)
+print("R-squared (R2):", r2)
